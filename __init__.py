5386c37a200ab22c1fee70e6727cc2f986c47d47
David Edson <eddo888@tpg.com.au>
2014-02-27 14:47:53 +1100

    Update __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# shared tools for the CGU project
-
-# $Date: 2013-12-13 01:22:40 +1100 (Fri, 13 Dec 2013) $
-# $Revision: 7279 $
-# $Author: david.edson $
-# $HeadURL: http://cgu-svn/CGUBPMLombardi/trunk/Scripts/__init__.py $
-# $Id: __init__.py 7279 2013-12-12 14:22:40Z david.edson $
