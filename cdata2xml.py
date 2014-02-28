fe2a9d26018864c22436102d443413304695073f
David Edson <eddo888@tpg.com.au>
2014-02-28 14:03:20 +1100

    Update cdata2xml.py

--- cdata2xml.py
+++ cdata2xml.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 
-# $Date: 2013-12-13 01:22:40 +1100 (Fri, 13 Dec 2013) $
-# $Revision: 7279 $
-# $Author: david.edson $
-# $HeadURL: http://cgu-svn/CGUBPMLombardi/trunk/Scripts/cdata2xml.py $
-# $Id: cdata2xml.py 7279 2013-12-12 14:22:40Z david.edson $
-
 import sys
 from _tools.cdata import *
 
