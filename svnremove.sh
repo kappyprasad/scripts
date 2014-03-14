c5cb0714b5422d50a78a904f8a7a0a9ca563dfd3
David Edson <eddo888@tpg.com.au>
2014-03-14 22:12:58 +1100

    Update svnremove.sh

--- svnremove.sh
+++ svnremove.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# $Date: 2014-03-03 16:38:15 +1100 (Mon, 03 Mar 2014) $
-# $Revision: 8986 $
-# $Author: david.edson $
-# $HeadURL: http://cgu-svn/CGUBPMLombardi/trunk/Scripts/svnremove.sh $
-# $Id: svnremove.sh 8986 2014-03-03 05:38:15Z david.edson $
-
 echo=""
 
 if [ "$1" = "-v" ]
