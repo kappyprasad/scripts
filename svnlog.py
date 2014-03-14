e252831d7f2c8fef15426979efec62644263f0cd
David Edson <eddo888@tpg.com.au>
2014-03-14 22:11:58 +1100

    Update svnlog.py

--- svnlog.py
+++ svnlog.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python
 
-# $Date: 2014-03-11 19:20:01 +1100 (Tue, 11 Mar 2014) $
-# $Revision: 9247 $
-# $Author: david.edson $
-# $HeadURL: http://cgu-svn/CGUBPMLombardi/trunk/Scripts/svnlog.py $
-# $Id: svnlog.py 9247 2014-03-11 08:20:01Z david.edson $
-
-
 import sys,re,os
 
 from _tools.eddo import *
