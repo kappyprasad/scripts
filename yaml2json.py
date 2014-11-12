0323033daf18defb7206486739a9115eb0c642c7
David Edson <david.edson@sirca.org.au>
2014-11-13 10:10:15 +1100

    clean

--- yaml2json.py
+++ yaml2json.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
-import os,re,sys,argparse,yaml,json
+from _tools.pretty import prettyPrint
+import os, re, sys, argparse, yaml, json
+
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-v','--verbose', action='store_true', help='show detailed output')
 parser.add_argument('-r','--reverse', action='store_true', help='reverse json->yaml')
-parser.add_argument('file',           action='store',      help='file to parse', nargs='*')
+parser.add_argument('-o','--output',  action='store',      help='output file name')
+parser.add_argument('-i','--input',    action='store',      help='file to parse')
 
 args = parser.parse_args()
 
@@ -21,13 +24,25 @@ def unicode_representer(dumper, uni):
 yaml.add_representer(unicode, unicode_representer)
 
 def main():
-    for file in args.file:
-        with open(file) as fp:
-            if args.reverse:
-                object = json.loads(''.join(fp.readlines()))
-                print yaml.dump(object)
-            else:
-                object = yaml.load(fp)
-                print json.dumps(object)
+    if args.output:
+        output = open(args.output,'w')
+    else:
+        output = sys.stdout
+        
+    if args.input:
+        input = open(args.input)
+    else:
+        input = sys.stdin
+        
+    if args.reverse:
+        object = json.load(input)
+        yaml.dump(object, stream=output)
+    else:
+        object = yaml.load(input)
+        json.dump(object, output)
 
+    output.close()
+    input.close()
+    
+    return
 if __name__ == '__main__': main()
