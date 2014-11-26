f1aec4c6157e887ae5a6eec4a4c5bea1863a6367
David Edson <david.edson@sirca.org.au>
2014-11-26 14:31:05 +1100

    with dict/eval

--- yaml2json.py
+++ yaml2json.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
-from _tools.pretty import prettyPrint
 import os, re, sys, argparse, yaml, json
+
+from _tools.pretty import prettyPrint
+from _tools.pyson import *
 
 
 parser = argparse.ArgumentParser()
@@ -9,7 +11,12 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-v','--verbose', action='store_true', help='show detailed output')
 parser.add_argument('-r','--reverse', action='store_true', help='reverse json->yaml')
 parser.add_argument('-o','--output',  action='store',      help='output file name')
-parser.add_argument('-i','--input',    action='store',      help='file to parse')
+parser.add_argument('-i','--input',   action='store',      help='files to parse', nargs='*', default=[])
+parser.add_argument('file',           action='store',      help='files to parse', nargs='*')
+
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-e','--eval',    action='store',      help='evaluate a JS expression string', metavar='\\[key\\[...')
+group.add_argument('-d','--dict',    action='store',      help='evaluate a JS object dict string', metavar='key.key...')
 
 args = parser.parse_args()
 
@@ -23,26 +30,43 @@ def unicode_representer(dumper, uni):
 
 yaml.add_representer(unicode, unicode_representer)
 
+def query(object):
+    if args.dict:
+        expression = dict2eval(args.dict)
+        if args.verbose:
+            sys.stderr.write('expression=object%s\n'%expression)
+        object = eval('object%s'%expression)
+    if args.eval:
+        object = eval('object%s'%args.eval)
+    return object
+    
+def process(input,output):
+    if args.reverse:
+        object = json.load(input)
+        object = query(object)
+        yaml.dump(object,stream=output,indent=4,default_flow_style=False)
+    else:
+        object = yaml.load(input)
+        object = query(object)
+        json.dump(object, output, indent=4)
+    input.close()
+    return
+
 def main():
     if args.output:
         output = open(args.output,'w')
     else:
         output = sys.stdout
-        
-    if args.input:
-        input = open(args.input)
-    else:
-        input = sys.stdin
-        
-    if args.reverse:
-        object = json.load(input)
-        yaml.dump(object,stream=output,indent=4,default_flow_style=False)
-    else:
-        object = yaml.load(input)
-        json.dump(object, output, indent=4)
 
+    files = args.input + args.file
+    if len(files) == 0:
+        process(sys.stdin,output)
+    else:
+        for file in files:
+            process(open(file),output)
+        
     output.close()
-    input.close()
     
     return
+
 if __name__ == '__main__': main()
