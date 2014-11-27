189b5bfb6360f9665ef74f72e940872b7dc108c7
David Edson <david.edson@sirca.org.au>
2014-11-27 11:31:55 +1100

    all or modified

--- gitadd.sh
+++ gitadd.sh
@@ -7,6 +7,14 @@ then
     echo=echo
 fi
 
-git status --porcelain | egrep "^( M|\?\?)" | cut -c 4- | xargs -r -I FILE $echo git add "FILE"
+if [ "$1" = "-a" ]
+then
+    query="^( M|\?\?)"
+else
+    query="^( M)"
+fi
+
+
+git status --porcelain | egrep "$query" | cut -c 4- | xargs -r -I FILE $echo git add "FILE"
 
 
