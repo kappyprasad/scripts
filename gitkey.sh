2f088d5885ac4f1ad0f5669c8225940ca3eb5cfa
David Edson <david.edson@sirca.org.au>
2015-01-14 10:51:37 +1100

    generate public key for git from ssh

---
+++ gitkey.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+private="$1"
+
+if [ -z "$private" ]
+then
+    echo "usage: $0 <privateKey>"
+    exit 1
+fi
+
+if [ -f "$private.pub" ]
+then
+    echo "public key $private.pub already exists, exiting"
+    exit 1
+fi
+
+ssh-keygen -y -f "$private" > "$private.pub"
+
+ls "$private.pub"
