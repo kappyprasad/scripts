b66dd834ccb6f33323c64503aa0a16bbc8ff7fee
David Edson <david.edson@sirca.org.au>
2015-01-27 10:15:53 +1100

    indenting

--- gitupdate.sh
+++ gitupdate.sh
@@ -4,10 +4,10 @@ for repo in *
 do
     if [ -d "$repo" ] && [ ! "$repo" = "." ]
     then
-	pushd "$repo" > /dev/null
-	pwd
-	git pull
-	popd > /dev/null
+	    pushd "$repo" > /dev/null
+	    pwd
+	    git pull
+	    popd > /dev/null
     fi
 done
 
