746c96adbdbed98f4021a9e3996b103b87243e26
David Edson <eddo888@tpg.com.au>
2015-02-10 11:17:52 +1100

    revert with subdirs

---
+++ gitrevert.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+read -p "are you sure (yes/no) ? " sure
+
+if [ "$sure" != "yes" ]
+then
+    exit 1
+fi
+
+if [ -d .git ]
+then
+    repos=$(pwd)
+else
+    repos=*
+fi
+
+for repo in $repos
+do
+    if [ -d "$repo" ] && [ ! "$repo" = "." ]
+    then
+	    pushd "$repo" > /dev/null
+	    pwd
+	    git status --porcelain | cut -c 4- | xargs -rn1 git checkout
+	    popd > /dev/null
+    fi
+done
