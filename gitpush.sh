c7a6157a8da37d2e6543c72130afb59806c973f5
David Edson <eddo888@tpg.com.au>
2015-06-10 22:37:21 +1000

    latest

--- gitpush.sh
+++ gitpush.sh
@@ -1,6 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# bulk pusher
+help="\
+usage: $(basename $0)\n\
+\n\
+-v verbose\n\
+-h help\n\
+"
+
+verbose=0
+
+while getopts vh opt
+do
+    case $opt in
+        v) 
+            verbose=1
+            ;;
+        h) 
+            echo "$help"
+            exit 0
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
 
 if [ -d .git ]
 then
@@ -14,11 +36,17 @@ do
     if [ -d "$repo" ] && [ ! "$repo" = "." ]
     then 
         pushd $repo > /dev/null
-        pwd
+
+        if [ "$verbose" = "1" ]
+        then
+            pwd
+        fi
+
         if git status | grep "use \"git push\" to publish your local commits" >/dev/null
         then
             git push
         fi
+
         popd >/dev/null
     fi
 done
