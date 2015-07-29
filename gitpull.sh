9a9eeab2fbc09578541985080252fdc2088f42a5
David Edson <eddo888@tpg.com.au>
2015-07-29 14:59:15 +1000

    recurse

--- gitpull.sh
+++ gitpull.sh
@@ -1,29 +1,57 @@
 #!/usr/bin/env sh
 
-if [ -d .git ]
-then
-    local=1
-    repos=$(pwd)
-else
-    local=0
-    repos=*
-fi
+help="\
+usage: $(basename $0) <repos...>\n\
+\n\
+-v verbose\n\
+-h help\n\
+-r recurse\n\
+"
 
-for repo in $repos
+verbose=''
+recurse=''
+
+while getopts vhr opt
 do
-    if [ -d "$repo" ] && [ ! "$repo" = "." ]
-    then
-	    pushd "$repo" > /dev/null
-        if [ -d ".git" ]
-        then
-            if [ "$local" = "0" ]
-            then
-                echo "\033[36m$repo\033[0m"
-            fi
-	    git pull
-        fi
-	    popd > /dev/null
-    fi
+    case $opt in
+        v) 
+            verbose='-v'
+            ;;
+        h) 
+            echo -e "$help"
+            exit 0
+            ;;
+        r)
+            recurse="-r"
+            ;;
+    esac
 done
 
+shift $((OPTIND-1))
+
+repo="$1"
+
+if [ -z "$repo" ] && [ "$recurse" = "-r" ]
+then
+    find . -name .git -and -type d -exec $0 $verbose $recurse {} \;
+else
+    if [ "$recurse" = "-r" ]
+    then
+        repo=$(dirname $repo)
+    else
+        repo=.
+    fi
+
+    pushd $repo > /dev/null
+
+    if [ "$verbose" = "-v" ]
+    then
+        horizontal.pl
+        echo "\033[36m$repo\033[0m"
+    fi
+
+    git pull
+    
+    popd >/dev/null
+fi
 
