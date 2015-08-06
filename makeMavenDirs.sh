dce5ef60bb5f52593273c81d6aa15fb4013c200b
David Edson <eddo888@tpg.com.au>
2015-08-06 12:11:52 +1000

    latest

---
+++ makeMavenDirs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+
+mkdir -p src/main/java
+mkdir -p src/main/resources
+mkdir -p src/test/java
+mkdir -p src/test/resources
---
+++ mvnCreate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mvn archetype:generate -DarchetypeArtifactId=maven-archetype-quickstart
