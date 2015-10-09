7f3c7d125cfe11f866a23a0f8bfaa53bc1501f14
david.edson <david.edson@intelligentpathways.com.au>
2015-10-09 11:50:40 +1100

    ignore symlinks

--- rsync.pl
+++ rsync.pl
@@ -3,6 +3,9 @@
 while (<>) {
     if (/^sending incremental file list$/) {
     }
+    elsif (/^skipping non-regular file "([^"]+)"/) {
+        #print "~$1\n";
+    }
     elsif (/^sent\s[0-9\,]+\s.*$/) {
     }
     elsif (/^total size is [0-9\,]+\s.*$/) {
