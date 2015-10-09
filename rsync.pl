515c46f8398d1c3a32f8d7f51934d7ca06ea2d2d
david.edson <david.edson@intelligentpathways.com.au>
2015-10-09 11:45:23 +1100

    nice

--- rsync.pl
+++ rsync.pl
@@ -1,7 +1,17 @@
 #!/usr/bin/env perl
 
 while (<>) {
-    if ( ! /^(sending incremental file list|sent\s[0-9\,]+\s.*|total size is [0-9\,]+\s.*|\.\/|\s+)$/) {
+    if (/^sending incremental file list$/) {
+    }
+    elsif (/^sent\s[0-9\,]+\s.*$/) {
+    }
+    elsif (/^total size is [0-9\,]+\s.*$/) {
+    }
+    elsif (/^\.\/$/) {
+    }
+    elsif (/^\s+$/) {
+    }
+    else {
         print "+$_";
     }
 }
