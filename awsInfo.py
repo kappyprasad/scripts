567c09231b8e30cfff36f1cde1501dc099cfdc90
David Edson <eddo888@tpg.com.au>
2015-12-18 21:54:35 +0000

    nested

--- awsInfo.py
+++ awsInfo.py
@@ -2,19 +2,21 @@
 
 import os,sys,re,requests,json
 
-base='http://169.254.169.254/latest/meta-data'
+base='http://169.254.169.254/latest'
 
-def get(path):
+def get(base,path):
+    #sys.stderr.write('base="%s" path="%s"\n'%(base,path))
     response = requests.get('%s/%s'%(base,path))
-    return response.text
+    data = {}
+    if path.endswith('/'):
+        for key in response.text.split('\n'):
+            data[key] = get('%s/%s'%(base,path),key)
+    else:
+        data = response.text
+    return data
 
 def main():
-    keys = get('').split('\n')
-
-    data = {}
-    for key in keys:
-        data[key] = get(key)
-
+    data = get(base,'meta-data/')
     json.dump(data,sys.stdout,indent=4)
         
 if __name__ == '__main__': main()
