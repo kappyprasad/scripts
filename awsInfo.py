6b05594e49cb0810984e2e0b85c1debc4dbe4415
David Edson <david.edson@intelligentpathways.com.au>
2015-12-18 00:55:18 +0000

    2015-12-18T00:55:18

--- awsInfo.py
+++ awsInfo.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
-base=http://169.254.169.254/latest/meta-data
+import os,sys,re,requests,json
+
+base='http://169.254.169.254/latest/meta-data'
+
+def get(path):
+    response = requests.get('%s/%s'%(base,path))
+    return response.text
+
+def main():
+    keys = get('').split('\n')
+
+    data = {}
+    for key in keys:
+        data[key] = get(key)
+
+    json.dump(data,sys.stdout,indent=4)
+        
+if __name__ == '__main__': main()
+
+        
+    
 
 
