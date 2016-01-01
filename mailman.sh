b555d7712eb0153b3ff5530647b2c556f58b0343
david.edson <david.edson@intelligentpathways.com.au>
2016-01-01 14:03:52 +1100

    yay

--- mailman.sh
+++ mailman.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-mailman.py \
-    -s \
-    -j "Test Subject" \
-    -b "Test Body" \
-    -t eddo888@tpg.com.au david.edson@gmail.com \
-    -f _test/DavidEdson-CorpratePhoto.png _test/DavidEdson-hand.jpeg
+if [ -z "$1" ]
+then
+    echo "usage: $(basename $0) [sr]"
+    exit
+fi
+
+if [ "$1" = "-s" ]
+then
+    mailman.py \
+        -s \
+        -j "Test Subject" \
+        -b "Test Body" \
+        -t eddo888@tpg.com.au \
+        -f _test/DavidEdson-hand.jpeg
+fi
+
+if [ "$1" = "-r" ]
+then
+    mailman.py \
+        -r 
+
+fi
