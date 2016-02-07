acde082dd9183c0dc587d2611093d49a6321c137
david.edson <david.edson@intelligentpathways.com.au>
2016-02-08 10:53:19 +1100

    argue

--- parameters.py
+++ parameters.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 
-import os,sys,re,json
-import argparse
+import os,sys,re,json,argparse
 
-parser = argparse.ArgumentParser()
+def argue():
+    parser = argparse.ArgumentParser()
 
-parser.add_argument('-v','--verbose',action='store_true')
-parser.add_argument('-u','--username',action='store',help='fake user name')
-parser.add_argument('-p','--password',action='store',help='fake pass word')
-parser.add_argument('filename',action='store',help='the file name')
+    parser.add_argument('-v','--verbose',action='store_true')
+    parser.add_argument('-u','--username',action='store',help='fake user name')
+    parser.add_argument('-p','--password',action='store',help='fake pass word')
+    parser.add_argument('filename',action='store',help='the file name')
 
-group1=parser.add_mutually_exclusive_group(required=True)
-group1.add_argument('-x', '--ixml',      action='store',    help='xml input file')
-group1.add_argument('-X', '--oxml',      action='store',    help='xml output file')
+    group1=parser.add_mutually_exclusive_group(required=True)
+    group1.add_argument('-x', '--ixml',      action='store',    help='xml input file')
+    group1.add_argument('-X', '--oxml',      action='store',    help='xml output file')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-json.dump(vars(args),sys.stderr,indent=4)
-sys.stderr.write('\n')
+    if args.verbose:
+        sys.stderr.write('%s\n'%json.dumps(vars(args),indent=4))
 
+    return args
+
+args = argue()
 print 'verbose =',args.verbose
 print 'username=',args.username
 print 'password=',args.password
