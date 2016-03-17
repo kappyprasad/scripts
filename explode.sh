81bc28d9484264d9e1c4816fe19e4ad4e97adbbb
David Edson <davidedson@intelligentpathways.com.au>
2016-03-17 14:12:11 +1100

    explode

---
+++ explode.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+file="$1"
+
+if [ ! -f "$file" ]
+then
+    echo "useage: $(basename $0) <zip>"
+    exit 1
+fi
+
+leader=$(echo $file | cut -c -1)
+echo "leader=$leader"
+if [ "$leader" = "/" ] || "$leader" = "~" ]
+then
+    source="$file"
+else
+    source="$(pwd)/$file"
+fi
+
+target="$(basename "$file").exploded"
+
+#echo "source=$source"
+#echo "target=$target"
+
+if [ -d "$target" ]
+then
+    echo "$target already exists"
+    exit 1
+fi
+
+mkdir "$target"
+pushd "$target"
+unzip -q "$source"
+popd
