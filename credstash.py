ed9dde1cc3a67a080e175cd43cb77c67d86cb1ca
David Edson <eddo888@tpg.com.au>
2017-05-04 17:42:40 +1000

    named table

--- credstash.py
+++ credstash.py
@@ -10,6 +10,16 @@ args = Argue()
 @args.command(single=True)
 class MyCredStash(CredStash):
 
+    @args.attribute(short='v',flag=True)
+    def verbose(self): return False
+
+    @args.attribute(short='t')
+    def table(self):
+        '''
+        the table name
+        '''
+        return args.super(MyCredStash,self).table()
+    
     @args.attribute(short='r')
     def region(self):
         '''
@@ -18,9 +28,17 @@ class MyCredStash(CredStash):
         return args.super(MyCredStash,self).region()
 
     @args.operation
+    def setup(self):
+        '''
+        call the setup for credstash
+        '''
+        return args.super(MyCredStash,self).setup()
+    
+    @args.operation
     def get(self,name):
         '''
         get a KMS key
+
         '''
         return args.super(MyCredStash,self).get(name)
 
@@ -28,6 +46,7 @@ class MyCredStash(CredStash):
     def put(self,name,value):
         '''
         put a KMS name,value key
+
         '''
         return args.super(MyCredStash,self).put(name,value)
 
@@ -45,8 +64,29 @@ class MyCredStash(CredStash):
         '''
         return '\n'.join(args.super(MyCredStash,self).list())
 
+    @args.operation(name="export")
+    def exporter(self, output):
+        values=dict()
+        for name in self.list().split('\n'):
+            print name
+            values[name]=self.get(name)
+        with open(output,'w') as fo:
+            json.dump(values,fo,indent=4)
+        return
+
+    @args.operation(name="import")
+    def importer(self,input):
+        with open(input) as fi:
+            values=json.load(fi)
+            for name in values.keys():
+                print name
+                self.put(name,values[name])
+        return
+    
 if __name__ == '__main__':
-    print args.execute()
+    result = args.execute()
+    if result:
+        print result
     
 
 
